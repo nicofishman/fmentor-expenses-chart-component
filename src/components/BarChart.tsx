8d0880854bac517c484f34da25cac8714496f6f5
nicofishman <nicourifishman@gmail.com>
2022-06-19 22:27:23 -0300

    Mobile cositos de precio

--- src/components/BarChart.tsx
+++ src/components/BarChart.tsx
@@ -16,7 +16,7 @@ const BarChart: FC<{
                             className={`peer ${isDay ? 'bg-cyan' : 'bg-soft-red'} sm:w-14 w-8 flex flex-col justify-end rounded-md hover:opacity-70 cursor-pointer`}
                             style={{ height }}
                         />
-                        <div className='z-10 flex justify-center text-white bg-dark-brown items-center text-xs sm:text-lg text-center sm:p-2 px-2 py-1 sm:w-full w-[56px] h-10 rounded-lg absolute -top-12 origin-bottom invisible peer-hover:visible'>${day.amount}</div>
+                        <div className='z-10 flex justify-center text-white bg-dark-brown items-center text-xs sm:text-lg text-center sm:py-4 sm:px-10 px-2 py-1 sm:w-full w-[56px] h-10 rounded-lg absolute -top-12 origin-bottom invisible peer-hover:visible'>${day.amount}</div>
                         <div className='sm:text-sm text-xs text-center font-extralight text-medium-brown'>{day.day}</div>
                     </div>
                 );
